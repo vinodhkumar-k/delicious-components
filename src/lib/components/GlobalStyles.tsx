306488cb4ad9a93fd96fe38c860b5e5aa6fa8845
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-06-26 20:55:43 +0530

    Update GlobalStyles.tsx

---
+++ src/lib/components/GlobalStyles.tsx
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+import css from '@styled-system/css';
+import { media } from 'styled-bootstrap-grid';
+
+const GlobalStyles = createGlobalStyle``;
+
+export default GlobalStyles;
