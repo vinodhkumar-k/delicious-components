88566f0673cc5453468024d6ee4d9a508895d721
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-06-26 23:08:33 +0530

    Update GlobalStyles.tsx

--- src/lib/components/GlobalStyles.tsx
+++ src/lib/components/GlobalStyles.tsx
@@ -2,6 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 import css from '@styled-system/css';
 import { media } from 'styled-bootstrap-grid';
 
-const GlobalStyles = createGlobalStyle``;
+const GlobalStyles = createGlobalStyle`
+  body {
+    ${css({
+      margin: 0,
+      fontFamily: 'secondary',
+      fontSize: 'sm',
+      lineHeight: 'sm',
+      backgroundColor: 'neutral_100',
+      color: 'text'
+    })}
+  }
+`;
 
 export default GlobalStyles;
