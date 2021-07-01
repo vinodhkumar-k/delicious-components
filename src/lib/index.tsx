2076648aa731e088c2ab46dbc4854ff31d14a1c8
Vinodh Kumar Konda <53635507+vinodhkumar-k@users.noreply.github.com>
2021-07-01 14:03:13 +0530

    Update index.tsx

---
+++ src/lib/index.tsx
@@ -0,0 +1,27 @@
+import Box from './components/Box';
+import Button from './components/Button';
+import Flex from './components/Flex';
+import GlobalStyles from './components/GlobalStyles';
+import { Container, Row, Col } from './components/Grid';
+import Icon from './components/Icon';
+import IconButton from './components/IconButton';
+import List from './components/List';
+import Text from './components/Text';
+import ThemeProvider from './components/ThemeProvider';
+import Tooltip from './components/Tooltip';
+
+export {
+  Box,
+  Button,
+  Flex,
+  GlobalStyles,
+  Container,
+  Row,
+  Col,
+  Icon,
+  IconButton,
+  List,
+  Text,
+  ThemeProvider,
+  Tooltip
+};
